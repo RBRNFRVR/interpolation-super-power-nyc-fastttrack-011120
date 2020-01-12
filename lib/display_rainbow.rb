ab107f31e68f47d9a36868464b4e705c07bc3270
Joseph Chavez <rommelchavez99@gmail.com>
2020-01-12 17:31:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
-# Write your #display_rainbow method here
-#display_rainbow
+
  def display_rainbow(['red','orange','yellow','green','blue','indigo','violet'])
    display_rainbow=["r","o","y","g","b","i","v"]
    puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[r]}"
