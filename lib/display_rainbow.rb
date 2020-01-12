2e0bdc5400ce896973086dfffaeee83f41568ef6
Joseph Chavez <rommelchavez99@gmail.com>
2020-01-12 17:34:30 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
-# Write your #display_rainbow method here
+
+ def display_rainbow(['red','orange','yellow','green','blue','indigo','violet'])
+   display_rainbow=["r","o","y","g","b","i","v"]
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[0]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[1]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[2]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[3]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[4]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[5]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[6]}"
+end
