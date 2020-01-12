f5f9ce264a8791444938196c9954288a993995f9
Joseph Chavez <rommelchavez99@gmail.com>
2020-01-12 17:34:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,12 @@
 
  def display_rainbow(['red','orange','yellow','green','blue','indigo','violet'])
    display_rainbow=["r","o","y","g","b","i","v"]
-   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[r]}"
-   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[o]}"
-   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[y]}"
-   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[g]}"
-   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[b]}"
-   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[i]}"
-   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[v]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[0]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[1]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[2]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[3]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[4]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[5]}"
+   puts "THE FIRST COLOR OF THE RAINBOW IS: #{display_rainbow[6]}"
 end
 display_rainbow(color)
